d5df97d6d787367dffd4ec7f7592f3c15fc651b6
einarstensson <einar.stensson@gmail.com>
2016-06-18 17:01:25 -0400

    Made some major styling chnages to the blog

--- spec/features/home_page_comments_spec.rb
+++ spec/features/home_page_comments_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 RSpec.describe "Home page coments", type: :feature do
 
   describe "Each front page post has a comments section" do
-    it "has a create comment form" do
+    before :each do
       visit '/'
+    end
+
+    it "has a create comment form." do
       expect(page.has_css?('form')).to be_truthy
     end
-   end
- end
+
+    it "has a comment section." do
+      post = Post.last
+      selector = "div[post_id_#{post.id}]"
+      expect(page.has_css?(selector)).to be_truthy
+    end
+
+    it "that belongs to the post on display." do
+      post = Post.last
+      comment_section_id = page.find("div#comments")["post_id_" + post.id.to_s]
+      expect(comment_section_id).to be_truthy
+    end
+  end
+end
